6e1d9bf4c68d39c25d3702c136f7e59f1ae32c93
woxi01 <rohit.woxi@gmail.com>
2017-07-03 17:31:32 +0530

    splash login setup in progress

--- app/src/main/java/com/android/peticash/PetiCashListActivity.java
+++ app/src/main/java/com/android/peticash/PetiCashListActivity.java
@@ -1,7 +1,8 @@
 package com.android.peticash;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
 
 import com.android.constro360.R;
 
@@ -10,5 +11,8 @@ public class PetiCashListActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_peti_cash_list);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        toolbar.setTitle(getString(R.string.app_name));
+        setSupportActionBar(toolbar);
     }
 }
