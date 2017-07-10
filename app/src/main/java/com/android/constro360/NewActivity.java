cbc00d4138c30b468608977c8c77c20b3b79123c
woxi01 <rohit.woxi@gmail.com>
2017-07-10 12:41:54 +0530

    added horizontal selector module customization in progress

--- app/src/main/java/com/android/constro360/NewActivity.java
+++ app/src/main/java/com/android/constro360/NewActivity.java
@@ -2,6 +2,7 @@ package com.android.constro360;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.Menu;
 import android.view.MenuItem;
 
 public class NewActivity extends AppCompatActivity {
@@ -16,17 +17,23 @@ public class NewActivity extends AppCompatActivity {
         getSupportActionBar().setTitle("July 2017  ˅        Add Material");*/
         /*setContentView(R.layout.activity_purchase_request_list);
         getSupportActionBar().setTitle("July 2017  ˅");*/
-
         /*setContentView(R.layout.activity_purchase_process);
-        getSupportActionBar().setTitle("Purchase Summary");*/
-        /*setContentView(R.layout.activity_pay_with_peticash);
-        getSupportActionBar().setTitle("Pay with Paticash");*/
+        getSupportActionBar().setTitle("PR2546565641");*/
+        setContentView(R.layout.activity_pay_with_peticash);
+        getSupportActionBar().setTitle("PO437567575");
         /*setContentView(R.layout.activity_inventory_list);
         getSupportActionBar().setTitle("Inventory               July 2017  ˅");*/
-        setContentView(R.layout.activity_inventory_details);
-        getSupportActionBar().setTitle("Details                    July 2017 ˅");
+        /*setContentView(R.layout.activity_inventory_details);
+        getSupportActionBar().setTitle("Details                    July 2017 ˅");*/
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
     }
+
+    /*@Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.options_menu, menu);
+        return true;
+    }*/
 
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
