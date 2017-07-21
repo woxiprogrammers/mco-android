74c5fc0de8d150b98a06afb20fe70cf823a45849
woxi01 <rohit.woxi@gmail.com>
2017-07-21 13:05:16 +0530

    login architecture testing

---
+++ app/src/test/java/com/android/login_mvp/LoginInteractorTest.java
@@ -0,0 +1,15 @@
+package com.android.login_mvp;
+
+import org.junit.Test;
+
+/**
+ * <b></b>
+ * <p>This class is used to </p>
+ * Created by Rohit.
+ */
+public class LoginInteractorTest {
+    @Test
+    public void login() throws Exception {
+//        assertEquals(4, 3 + 2);
+    }
+}
