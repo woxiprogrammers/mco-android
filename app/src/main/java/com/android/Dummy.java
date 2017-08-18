3a3aeb7a296834e355a451b239322a6581b92933
Sharvari <sharvari.woxi@gmail.com>
2017-08-18 15:24:26 +0530

    Completed UI design for dialog form for multiple materials

---
+++ app/src/main/java/com/android/Dummy.java
@@ -0,0 +1,15 @@
+package com.android;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.android.constro360.R;
+
+public class Dummy extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.layout_open_dialog_on_materials);
+    }
+}
