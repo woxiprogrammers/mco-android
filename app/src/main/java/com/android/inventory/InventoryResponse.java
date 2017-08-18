e322ee9eeb39e66e1e123ebc30ae4b71f451fc2d
Sharvari <sharvari.woxi@gmail.com>
2017-08-18 17:37:07 +0530

    Completed UI design for dialog form for multiple materials

---
+++ app/src/main/java/com/android/inventory/InventoryResponse.java
@@ -0,0 +1,41 @@
+package com.android.inventory;
+
+import com.google.gson.annotations.SerializedName;
+
+import io.realm.RealmObject;
+
+public class InventoryResponse extends RealmObject {
+
+	@SerializedName("inventoryDataResponse")
+	private InventoryDataResponse inventoryDataResponse;
+
+	@SerializedName("next_url")
+	private String nextUrl;
+
+	@SerializedName("message")
+	private String message;
+
+	public void setInventoryDataResponse(InventoryDataResponse inventoryDataResponse){
+		this.inventoryDataResponse = inventoryDataResponse;
+	}
+
+	public InventoryDataResponse getInventoryDataResponse(){
+		return inventoryDataResponse;
+	}
+
+	public void setNextUrl(String nextUrl){
+		this.nextUrl = nextUrl;
+	}
+
+	public String getNextUrl(){
+		return nextUrl;
+	}
+
+	public void setMessage(String message){
+		this.message = message;
+	}
+
+	public String getMessage(){
+		return message;
+	}
+}
---
+++ app/src/main/java/com/android/inventory/MaterialListing.java
@@ -0,0 +1,15 @@
+package com.android.inventory;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.android.constro360.R;
+
+public class MaterialListing extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_material_listing);
+    }
+}
