b57b373e5773471e29ea40741797c24b9b03e180
Sharvari <sharvari.woxi@gmail.com>
2017-08-30 12:15:18 +0530

    Working with UI modification for asset request maintainance

---
+++ app/src/main/java/com/android/inventory/assets/ActivityRequestMaintanance.java
@@ -0,0 +1,15 @@
+package com.android.inventory.assets;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+import com.android.constro360.R;
+
+public class ActivityRequestMaintanance extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_asset_maintenance);
+    }
+}
--- app/src/main/java/com/android/inventory/material/AssetListFragment.java
+++ app/src/main/java/com/android/inventory/material/AssetListFragment.java
@@ -3,6 +3,9 @@ package com.android.inventory.material;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
 
@@ -14,18 +17,18 @@ import com.android.interfaces.FragmentInterface;
  */
 public class AssetListFragment extends Fragment implements FragmentInterface {
 
+//    MenuInflater inflater = getActivity().getMenuInflater();
     public AssetListFragment() {
         // Required empty public constructor
     }
 
     public static AssetListFragment newInstance() {
-
         Bundle args = new Bundle();
-
         AssetListFragment fragment = new AssetListFragment();
         fragment.setArguments(args);
         return fragment;
     }
+
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
@@ -36,6 +39,32 @@ public class AssetListFragment extends Fragment implements FragmentInterface {
 
     @Override
     public void fragmentBecameVisible() {
+
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setHasOptionsMenu(true);//Make sure you have this line of code.
+    }
+
+    @Override
+    public void onCreateOptionsMenu(Menu menu, MenuInflater inflater) {
+        inflater.inflate(R.menu.options_menu, menu);
+        super.onCreateOptionsMenu(menu, inflater);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()){
+            case R.id.action_request_maintaianance:
+
+                break;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
+    private void startRequestMaintainanceActivity(){
 
     }
 }
