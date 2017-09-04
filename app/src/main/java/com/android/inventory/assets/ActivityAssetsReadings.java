da8b386d29776265a17c4183caa974449b711452
Sharvari <sharvari.woxi@gmail.com>
2017-09-04 12:55:51 +0530

    UI modification to add starting point for assets

--- app/src/main/java/com/android/inventory/assets/ActivityAssetsReadings.java
+++ app/src/main/java/com/android/inventory/assets/ActivityAssetsReadings.java
@@ -1,22 +1,65 @@
 package com.android.inventory.assets;
 
-import android.app.Activity;
 import android.content.Context;
 import android.os.Bundle;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.ImageButton;
+import android.widget.LinearLayout;
+import android.widget.Toast;
 
 import com.android.constro360.R;
+import com.android.utils.BaseActivity;
 
-public class ActivityAssetsReadings extends Activity {
+import butterknife.BindView;
+import butterknife.ButterKnife;
+import butterknife.OnClick;
+
+public class ActivityAssetsReadings extends BaseActivity {
+
+    @BindView(R.id.ll_add_readings)
+    LinearLayout llAddReadings;
+
+    @BindView(R.id.iamgeButton_open_readings_menu)
+    ImageButton iamgeButtonOpenReadingsMenu;
 
     private Context mContext;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_assets_readings);
+        ButterKnife.bind(this);
         initializeViews();
+        inflateReadingLayout();
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case android.R.id.home:
+                onBackPressed();
+                break;
+        }
+        return super.onOptionsItemSelected(item);
     }
 
     private void initializeViews() {
-        mContext=ActivityAssetsReadings.this;
+        mContext = ActivityAssetsReadings.this;
+        if (getSupportActionBar() != null) {
+            getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        }
+    }
+
+    private void inflateReadingLayout() {
+        View child = getLayoutInflater().inflate(R.layout.item_add_asset_readings, null);
+        llAddReadings.addView(child);
+        ButterKnife.bind(this,child);
+
+    }
+
+    @OnClick(R.id.iamgeButton_open_readings_menu)
+    public void onViewClicked() {
+        Toast.makeText(mContext,"Hello",Toast.LENGTH_SHORT).show();
     }
 }
