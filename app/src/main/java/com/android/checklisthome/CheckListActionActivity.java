ba062d092a952c98bc507e345c711123494ac409
Sharvari <sharvari.woxi@gmail.com>
2017-11-17 11:35:17 +0530

    Working with checklist UI design

---
+++ app/src/main/java/com/android/checklisthome/CheckListActionActivity.java
@@ -0,0 +1,12 @@
+package com.android.checklisthome;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class CheckListActionActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+}
