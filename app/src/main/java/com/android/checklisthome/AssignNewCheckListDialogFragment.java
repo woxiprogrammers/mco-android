224402a0aa4482bf901585ca11693833846c983c
RohitWoxi <rohit.woxi@gmail.com>
2017-11-21 14:32:23 +0530

    functionality category selection in progress

--- app/src/main/java/com/android/checklisthome/AssignNewCheckListDialogFragment.java
+++ app/src/main/java/com/android/checklisthome/AssignNewCheckListDialogFragment.java
@@ -7,6 +7,9 @@ import android.support.v4.app.DialogFragment;
 import android.support.v7.app.AlertDialog;
 import android.view.LayoutInflater;
 import android.view.View;
+import android.widget.Button;
+import android.widget.LinearLayout;
+import android.widget.Spinner;
 
 import com.android.constro360.R;
 
@@ -16,6 +19,15 @@ import com.android.constro360.R;
  * Created by Rohit.
  */
 public class AssignNewCheckListDialogFragment extends DialogFragment {
+    private Spinner mSpinnerChecklistCategories;
+    private Spinner mSpinnerChecklistSubCategories;
+    private Spinner mSpinnerChecklistFloorName;
+    private Spinner mSpinnerChecklistTitles;
+    private Spinner mSpinnerChecklistDescription;
+    private LinearLayout mLinearLayoutChecklistAssignTo;
+    private Button mButtonDismissAssignChecklistDialog;
+    private Button mButtonAssignChecklistDialog;
+
     public AssignNewCheckListDialogFragment() {
     }
 
@@ -37,8 +49,33 @@ public class AssignNewCheckListDialogFragment extends DialogFragment {
         // Get the layout inflater
         LayoutInflater inflater = getActivity().getLayoutInflater();
 //        Calendar cal = Calendar.getInstance();
-        View dialog = inflater.inflate(R.layout.date_picker_dialog, null);
-        builder.setView(dialog);
+        View dialogView = inflater.inflate(R.layout.dialog_add_new_checklist, null);
+        mSpinnerChecklistCategories = dialogView.findViewById(R.id.spinner_checklist_categories);
+        mSpinnerChecklistSubCategories = dialogView.findViewById(R.id.spinner_checklist_sub_categories);
+        mSpinnerChecklistFloorName = dialogView.findViewById(R.id.spinner_checklist_floor_name);
+        mSpinnerChecklistTitles = dialogView.findViewById(R.id.spinner_checklist_titles);
+        mSpinnerChecklistDescription = dialogView.findViewById(R.id.spinner_checklist_description);
+        mLinearLayoutChecklistAssignTo = dialogView.findViewById(R.id.linearLayout_checklist_assign_to);
+        mButtonDismissAssignChecklistDialog = dialogView.findViewById(R.id.button_dismiss_assign_checklist_dialog);
+        mButtonAssignChecklistDialog = dialogView.findViewById(R.id.button_assign_checklist_dialog);
+
+        /* HashMap<CheckBox, String> checkBoxMap = new HashMap<>();
+    checkBoxMap.put((CheckBox) findViewById(R.id.option1), "Option1");
+    checkBoxMap.put((CheckBox) findViewById(R.id.option2), "Option2");
+    checkBoxMap.put((CheckBox) findViewById(R.id.option3), "Option3");
+    checkBoxMap.put((CheckBox) findViewById(R.id.option4), "Option4");
+    checkBoxMap.put((CheckBox) findViewById(R.id.option5), "Option5");
+
+    CheckBoxGroup<String> checkBoxGroup = new CheckBoxGroup<>(checkBoxMap,
+        new CheckBoxGroup.CheckedChangeListener<String>() {
+          @Override
+          public void onOptionChange(ArrayList<String> options) {
+            Toast.makeText(MainActivity.this,
+                options.toString(),
+                Toast.LENGTH_LONG).show();
+          }
+        });*/
+        builder.setView(dialogView);
         return builder.create();
     }
 }
