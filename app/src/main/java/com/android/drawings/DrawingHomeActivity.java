5fa53be2dd9dbd5300b464f52f1affe603a6f11e
Sharvari <sharvari.woxi@gmail.com>
2017-11-28 14:20:15 +0530

    Drawing UI

--- app/src/main/java/com/android/drawings/DrawingHomeActivity.java
+++ app/src/main/java/com/android/drawings/DrawingHomeActivity.java
@@ -1,5 +1,6 @@
 package com.android.drawings;
 import android.os.Bundle;
+import android.view.MenuItem;
 
 import com.android.constro360.BaseActivity;
 import com.android.constro360.R;
@@ -10,5 +11,17 @@ public class DrawingHomeActivity extends BaseActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_drawing_home);
+        if(getSupportActionBar() != null){
+            getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+            getSupportActionBar().setTitle("Drawings Management");
+        }
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        if(item.getItemId() == android.R.id.home){
+            onBackPressed();
+        }
+        return super.onOptionsItemSelected(item);
     }
 }
