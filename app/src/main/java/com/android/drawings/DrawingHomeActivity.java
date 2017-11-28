8c73c16fd69d01944f3cb8e59a4a26cba7c0e244
Sharvari <sharvari.woxi@gmail.com>
2017-11-28 12:55:12 +0530

    Working with drawing module

---
+++ app/src/main/java/com/android/drawings/DrawingHomeActivity.java
@@ -0,0 +1,14 @@
+package com.android.drawings;
+import android.os.Bundle;
+
+import com.android.constro360.BaseActivity;
+import com.android.constro360.R;
+
+public class DrawingHomeActivity extends BaseActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_drawing_home);
+    }
+}
