6a4e361691ad9baa30440c94c1172eac7122dfd3
Sharvari <sharvari.woxi@gmail.com>
2017-11-29 12:41:30 +0530

    Drawing details activity

--- app/src/main/java/com/android/drawings/DrawingDetailsActivity.java
+++ app/src/main/java/com/android/drawings/DrawingDetailsActivity.java
@@ -1,17 +1,33 @@
 package com.android.drawings;
 
 import android.os.Bundle;
+import android.widget.FrameLayout;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 import com.android.constro360.BaseActivity;
 import com.android.constro360.R;
 
+import butterknife.BindView;
+import butterknife.ButterKnife;
+
 public class DrawingDetailsActivity extends BaseActivity {
+
+    @BindView(R.id.imageViewPreview)
+    ImageView imageViewPreview;
+    @BindView(R.id.textviewComments)
+    TextView textviewComments;
+    @BindView(R.id.textviewVersions)
+    TextView textviewVersions;
+    @BindView(R.id.frameLayout)
+    FrameLayout frameLayout;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.layout_drawing_details);
-        if(getSupportActionBar() != null){
+        ButterKnife.bind(this);
+        if (getSupportActionBar() != null) {
             getSupportActionBar().setDisplayHomeAsUpEnabled(true);
             getSupportActionBar().setTitle("Drawing Details");
         }
