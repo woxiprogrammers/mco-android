381a191ef6aa4e8626b62d90212a84a981c5215a
RohitWoxi <rohit.woxi@gmail.com>
2017-12-19 12:03:30 +0530

    adding FCM to project

---
+++ app/src/main/java/com/android/firebase/MyFirebaseInstanceIDService.java
@@ -0,0 +1,53 @@
+package com.android.firebase;
+
+import com.android.utils.AppURL;
+import com.android.utils.AppUtils;
+import com.androidnetworking.AndroidNetworking;
+import com.androidnetworking.common.Priority;
+import com.androidnetworking.error.ANError;
+import com.androidnetworking.interfaces.JSONObjectRequestListener;
+import com.google.firebase.iid.FirebaseInstanceId;
+import com.google.firebase.iid.FirebaseInstanceIdService;
+
+import org.json.JSONObject;
+
+import timber.log.Timber;
+
+/**
+ * <b></b>
+ * <p>This class is used to </p>
+ * Created by Rohit.
+ */
+public class MyFirebaseInstanceIDService extends FirebaseInstanceIdService {
+    @Override
+    public void onTokenRefresh() {
+        // Get updated InstanceID token.
+        String refreshedToken = FirebaseInstanceId.getInstance().getToken();
+        Timber.d("Refreshed token: " + refreshedToken);
+        // If you want to send messages to this application instance or
+        // manage this apps subscriptions on the server side, send the
+        // Instance ID token to your app server.
+        sendRegistrationToServer(refreshedToken);
+    }
+
+    private void sendRegistrationToServer(String refreshedToken) {
+        if (AppUtils.getInstance().isNetworkAvailable()) {
+            AndroidNetworking.post(AppURL.API_SEND_FIREBASE_REFRESHED_TOKEN + AppUtils.getInstance().getCurrentToken())
+                    .addBodyParameter("firebaseRefreshedToken", refreshedToken)
+                    .setTag("requestLatestAcl")
+                    .setPriority(Priority.MEDIUM)
+                    .build()
+                    .getAsJSONObject(new JSONObjectRequestListener() {
+                        @Override
+                        public void onResponse(JSONObject response) {
+                            Timber.d(String.valueOf(response));
+                        }
+
+                        @Override
+                        public void onError(ANError anError) {
+                            AppUtils.getInstance().logApiError(anError, "sendRegistrationToServer");
+                        }
+                    });
+        }
+    }
+}
