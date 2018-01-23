bfe8e1ab54ee2fb858873afc76c3e5b5c13a5408
Sharvari <sharvari.woxi@gmail.com>
2018-01-23 18:11:52 +0530

    Working with purchase order request material listing

--- app/src/main/java/com/android/purchase_module/purchase_request/PurchaseOrderMaterialRequestApproveActivity.java
+++ app/src/main/java/com/android/purchase_module/purchase_request/PurchaseOrderMaterialRequestApproveActivity.java
@@ -1,26 +1,42 @@
 package com.android.purchase_module.purchase_request;
 
-import android.app.Activity;
 import android.app.AlertDialog;
 import android.content.Context;
 import android.content.DialogInterface;
 import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
 
 import com.android.constro360.BaseActivity;
 import com.android.constro360.R;
+import com.android.inventory_module.inventory_model.MaterialListItem;
 import com.android.utils.AppUtils;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+import io.realm.OrderedRealmCollection;
+import io.realm.RealmRecyclerViewAdapter;
 
 public class PurchaseOrderMaterialRequestApproveActivity extends BaseActivity {
 
+    @BindView(R.id.rvList)
+    RecyclerView rvList;
     private Context mContext;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_purchase_order_material_request_approve);
+        ButterKnife.bind(this);
         initializeViews();
     }
+
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         getMenuInflater().inflate(R.menu.purchase_details_approve_menu, menu);
@@ -28,8 +44,8 @@ public class PurchaseOrderMaterialRequestApproveActivity extends BaseActivity {
     }
 
     private void initializeViews() {
-        mContext=PurchaseOrderMaterialRequestApproveActivity.this;
-        if(getSupportActionBar() != null){
+        mContext = PurchaseOrderMaterialRequestApproveActivity.this;
+        if (getSupportActionBar() != null) {
             getSupportActionBar().setDisplayHomeAsUpEnabled(true);
             getSupportActionBar().setTitle("");
         }
@@ -37,10 +53,10 @@ public class PurchaseOrderMaterialRequestApproveActivity extends BaseActivity {
 
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
-        if(android.R.id.home == item.getItemId()){
+        if (android.R.id.home == item.getItemId()) {
             onBackPressed();
         }
-        if(R.id.action_approve == item.getItemId()) {
+        if (R.id.action_approve == item.getItemId()) {
             if (AppUtils.getInstance().checkNetworkState()) {
                 openApproveDialog();
             } else {
@@ -69,4 +85,44 @@ public class PurchaseOrderMaterialRequestApproveActivity extends BaseActivity {
         alert.setTitle("Approve PO");
         alert.show();
     }
+
+    //ToDo item class
+    public class MaterialRequestListAdapter extends RealmRecyclerViewAdapter<MaterialListItem, MaterialRequestListAdapter.MyViewHolder> {
+        private OrderedRealmCollection<MaterialListItem> materialListItemCollection;
+
+        public MaterialRequestListAdapter(@Nullable OrderedRealmCollection<MaterialListItem> data, boolean autoUpdate, boolean updateOnModification) {
+            super(data, autoUpdate, updateOnModification);
+            materialListItemCollection = data;
+        }
+
+        @Override
+        public MyViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+            View itemView = LayoutInflater.from(parent.getContext()).inflate(R.layout.fragment_purchase_order_material_approve, parent, false);
+            return new MyViewHolder(itemView);
+        }
+
+        @Override
+        public void onBindViewHolder(MyViewHolder holder, int position) {
+            final MaterialListItem materialListItem = materialListItemCollection.get(position);
+
+        }
+
+        @Override
+        public long getItemId(int index) {
+            return materialListItemCollection.get(index).getId();
+        }
+
+        class MyViewHolder extends RecyclerView.ViewHolder {
+            @BindView(R.id.textView_item_name)
+            TextView textViewItemName;
+            @BindView(R.id.textView_item_quantity)
+            TextView textViewItemQuantity;
+
+            private MyViewHolder(View itemView) {
+                super(itemView);
+                ButterKnife.bind(this, itemView);
+            }
+        }
+    }
+
 }
