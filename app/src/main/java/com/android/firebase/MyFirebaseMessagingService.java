7d2923be7342bfd4be848546a19e282174ec915d
RohitWoxi <rohit.woxi@gmail.com>
2018-02-01 13:24:28 +0530

    upgrade to api level 27, latest oreo 8.1

--- app/src/main/java/com/android/firebase/MyFirebaseMessagingService.java
+++ app/src/main/java/com/android/firebase/MyFirebaseMessagingService.java
@@ -1,11 +1,14 @@
 package com.android.firebase;
 
+import android.app.NotificationChannel;
 import android.app.NotificationManager;
 import android.app.PendingIntent;
 import android.content.Context;
 import android.content.Intent;
+import android.graphics.Color;
 import android.media.RingtoneManager;
 import android.net.Uri;
+import android.os.Build;
 import android.support.v4.app.NotificationCompat;
 
 import com.android.constro360.R;
@@ -17,6 +20,8 @@ import com.google.firebase.messaging.RemoteMessage;
 import timber.log.Timber;
 
 public class MyFirebaseMessagingService extends FirebaseMessagingService {
+    private NotificationChannel notificationChannel;
+
     public MyFirebaseMessagingService() {
     }
 
@@ -31,21 +36,31 @@ public class MyFirebaseMessagingService extends FirebaseMessagingService {
     private void sendNotification(String messageBody) {
         Intent intent = new Intent(this, DashBoardActivity.class);
         intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
-        PendingIntent pendingIntent = PendingIntent.getActivity(this, AppConstants.FIREBASE_NOTIFICATION_ACTIVITY_REQUEST_CODE /* Request code */, intent,
+        PendingIntent pendingIntent = PendingIntent.getActivity(this, AppConstants.FCM_NOTIFICATION_ACTIVITY_REQUEST_CODE /* Request code */, intent,
                 PendingIntent.FLAG_ONE_SHOT);
         Uri defaultSoundUri = RingtoneManager.getDefaultUri(RingtoneManager.TYPE_NOTIFICATION);
+        NotificationManager notificationManager = (NotificationManager) getSystemService(Context.NOTIFICATION_SERVICE);
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            if (notificationManager != null) {
+                notificationChannel = new NotificationChannel(AppConstants.FCM_CHANNEL_ID,
+                        AppConstants.FCM_CHANNEL_NAME, NotificationManager.IMPORTANCE_DEFAULT);
+                notificationChannel.enableLights(true);
+                notificationChannel.enableVibration(true);
+                notificationChannel.setLightColor(Color.WHITE);
+                notificationChannel.setShowBadge(false);
+                notificationManager.createNotificationChannel(notificationChannel);
+            }
+        }
         NotificationCompat.Builder notificationBuilder =
-                new NotificationCompat.Builder(this)
+                new NotificationCompat.Builder(this, AppConstants.FCM_CHANNEL_ID)
                         .setSmallIcon(R.drawable.ic_notification)
                         .setContentTitle("MCon Notification")
                         .setContentText(messageBody)
                         .setAutoCancel(true)
                         .setSound(defaultSoundUri)
                         .setContentIntent(pendingIntent);
-        NotificationManager notificationManager =
-                (NotificationManager) getSystemService(Context.NOTIFICATION_SERVICE);
         if (notificationManager != null) {
-            notificationManager.notify(AppConstants.FIREBASE_NOTIFICATION_ID, notificationBuilder.build());
+            notificationManager.notify(AppConstants.FCM_NOTIFICATION_ID, notificationBuilder.build());
         }
     }
 }
