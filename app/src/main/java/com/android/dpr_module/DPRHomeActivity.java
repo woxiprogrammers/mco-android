d0aed02398227277b061a59d9814e5e6b4f0fcbd
Sharvari Desai <sharvari.woxi@gmail.com>
2018-06-19 17:35:41 +0530

    purchase order details change

--- app/src/main/java/com/android/dpr_module/DPRHomeActivity.java
+++ app/src/main/java/com/android/dpr_module/DPRHomeActivity.java
@@ -1,6 +1,9 @@
 package com.android.dpr_module;
 
 import android.content.Context;
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.os.Bundle;
 import android.support.v7.widget.CardView;
 import android.text.TextUtils;
@@ -10,6 +13,7 @@ import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ImageView;
 import android.widget.LinearLayout;
 import android.widget.Spinner;
 import android.widget.TextView;
@@ -21,6 +25,7 @@ import com.android.dpr_module.dpr_model.DPRSubContractorCatResponse;
 import com.android.dpr_module.dpr_model.DPRSubContractorResponse;
 import com.android.dpr_module.dpr_model.DprdataItem;
 import com.android.dpr_module.dpr_model.SubdataItem;
+import com.android.utils.AppConstants;
 import com.android.utils.AppURL;
 import com.android.utils.AppUtils;
 import com.androidnetworking.AndroidNetworking;
@@ -28,17 +33,24 @@ import com.androidnetworking.common.Priority;
 import com.androidnetworking.error.ANError;
 import com.androidnetworking.interfaces.JSONObjectRequestListener;
 import com.androidnetworking.interfaces.ParsedRequestListener;
+import com.vlk.multimager.activities.MultiCameraActivity;
+import com.vlk.multimager.utils.Constants;
+import com.vlk.multimager.utils.Image;
+import com.vlk.multimager.utils.Params;
 
 import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
 
+import java.io.File;
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
 import butterknife.OnClick;
+import id.zelory.compressor.Compressor;
 import io.realm.Realm;
 import io.realm.RealmResults;
 import timber.log.Timber;
@@ -50,10 +62,17 @@ public class DPRHomeActivity extends BaseActivity {
     Button buttonSubmit;
     @BindView(R.id.linearLayoutCategory)
     LinearLayout linearLayoutCategory;
+    @BindView(R.id.textViewCaptureDpr)
+    TextView textViewCaptureDpr;
+    @BindView(R.id.linearLayoutUploadedDprImage)
+    LinearLayout linearLayoutUploadedDprImage;
     private Context mContext;
     private Realm realm;
     private int intSubContId;
+    private ArrayList<File> arrayImageFileList;
     private RealmResults<DprdataItem> dprdataItemRealmResults;
+    private JSONArray jsonImageNameArray = new JSONArray();
+
 
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
@@ -70,6 +89,18 @@ public class DPRHomeActivity extends BaseActivity {
         } else {
             AppUtils.getInstance().showOfflineMessage("DPRHomeActivity");
         }
+    }
+    @OnClick(R.id.textViewCaptureDpr)
+    public void onViewClickedDprImage() {
+        Intent intent = new Intent(mContext, MultiCameraActivity.class);
+        Params params = new Params();
+        params.setCaptureLimit(AppConstants.IMAGE_PICK_CAPTURE_LIMIT);
+        params.setToolbarColor(R.color.colorPrimaryLight);
+        params.setActionButtonColor(R.color.colorAccentDark);
+        params.setButtonTextColor(R.color.colorWhite);
+        intent.putExtra(Constants.KEY_PARAMS, params);
+        startActivityForResult(intent, Constants.TYPE_MULTI_CAPTURE);
+
     }
 
     private void requestToSaveDetails() {
@@ -127,6 +158,44 @@ public class DPRHomeActivity extends BaseActivity {
                         AppUtils.getInstance().logRealmExecutionError(anError);
                     }
                 });
+    }
+
+    public void onActivityResult(int requestCode, int resultCode, Intent intent) {
+        super.onActivityResult(requestCode, resultCode, intent);
+        if (resultCode != RESULT_OK) {
+            return;
+        }
+        switch (requestCode) {
+            case Constants.TYPE_MULTI_CAPTURE:
+                ArrayList<Image> imagesList = intent.getParcelableArrayListExtra(Constants.KEY_BUNDLE_LIST);
+                addImages(imagesList, linearLayoutUploadedDprImage);
+
+                break;
+        }
+    }
+
+    private void addImages(ArrayList<Image> imagesList, LinearLayout layout) {
+        layout.removeAllViews();
+        arrayImageFileList = new ArrayList<File>();
+        for (Image currentImage : imagesList) {
+            if (currentImage.imagePath != null) {
+                File currentImageFile = new File(currentImage.imagePath);
+                arrayImageFileList.add(currentImageFile);
+                Bitmap myBitmap = BitmapFactory.decodeFile(currentImage.imagePath);
+                ImageView imageView = new ImageView(mContext);
+                LinearLayout.LayoutParams layoutParams = new LinearLayout.LayoutParams(200, 200);
+                layoutParams.setMargins(10, 10, 10, 10);
+                imageView.setLayoutParams(layoutParams);
+                imageView.setImageBitmap(myBitmap);
+                layout.addView(imageView);
+                imageView.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View view) {
+                        Toast.makeText(mContext, "Image Clicked", Toast.LENGTH_SHORT).show();
+                    }
+                });
+            }
+        }
     }
 
     @Override
@@ -293,4 +362,54 @@ public class DPRHomeActivity extends BaseActivity {
             linearLayoutCategory.addView(inflatedView);
         }
     }
+
+    private void uploadImages_addItemToLocal( final String imageFor) {
+        if (AppUtils.getInstance().checkNetworkState()) {
+            if (arrayImageFileList != null && arrayImageFileList.size() > 0) {
+                File sendImageFile = arrayImageFileList.get(0);
+                File compressedImageFile = sendImageFile;
+                try {
+                    compressedImageFile = new Compressor(mContext).compressToFile(sendImageFile);
+                } catch (IOException e) {
+                    Timber.i("IOException", "uploadImages_addItemToLocal: image compression failed");
+                }
+                String strToken = AppUtils.getInstance().getCurrentToken();
+                AndroidNetworking.upload(AppURL.API_IMAGE_UPLOAD_INDEPENDENT + strToken)
+                        .setPriority(Priority.MEDIUM)
+                        .addMultipartFile("image", compressedImageFile)
+                        .addMultipartParameter("image_for", imageFor)
+                        .addHeaders(AppUtils.getInstance().getApiHeaders())
+                        .setTag("uploadImages_addItemToLocal")
+                        .setPercentageThresholdForCancelling(50)
+                        .build()
+                        .getAsJSONObject(new JSONObjectRequestListener() {
+                            @Override
+                            public void onResponse(JSONObject response) {
+                                try {
+                                    String fileName = response.getString("filename");
+                                    jsonImageNameArray.put(fileName);
+                                } catch (JSONException e) {
+                                    e.printStackTrace();
+                                }
+                                try {
+                                    arrayImageFileList.remove(0);
+                                } catch (Exception e) {
+                                    e.printStackTrace();
+                                }
+//                                uploadImages_addItemToLocal(strTag, imageFor);
+                            }
+
+                            @Override
+                            public void onError(ANError anError) {
+                                AppUtils.getInstance().logApiError(anError, "uploadImages_addItemToLocal");
+                            }
+                        });
+            } else {
+
+            }
+        } else {
+            AppUtils.getInstance().showOfflineMessage("PayFragmentNew");
+        }
+    }
+
 }
